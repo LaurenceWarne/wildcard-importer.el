778bea987ea574641e15b78ccc419145c65e5d47
Laurence Warne <laurencewarne@gmail.com>
2023-04-09 20:39:36 +0100

    Implement buffer insertion

--- wildcard-importer.el
+++ wildcard-importer.el
@@ -19,7 +19,7 @@
 (defvar-local wildcard-importer-alist
   '((scala-mode
      ("import scala.math.Ordering.Implicits._" . "induced collection orderings")
-     ("import scala.concurrent.duration._" . "1.seconds, "))
+     ("import scala.concurrent.duration._" . "1.seconds, comparison ops (for java.util.concurrent.TimeUnit durations.)"))
     (python-mode
      "from collections import Counter")))
 
@@ -36,17 +36,31 @@
                  (format "%s    %s"
                          (make-string (- mx-width (length m)) ?\s)
                          helper)))))
-         (font-lock-kws font-lock-defaults))
-    (completing-read
-     "Import: "
-     (with-temp-buffer
-       (let ((font-lock-defaults font-lock-kws))
-         (mapcar (lambda (s)
-                   (erase-buffer)
-                   (insert s)
-                   (font-lock-ensure)
-                   (buffer-substring (point-min) (point-max)))
-                 import-strings))))))
+         (font-lock-kws font-lock-defaults)
+         (choice (completing-read
+                  "Import: "
+                  (with-temp-buffer
+                    (let ((font-lock-defaults font-lock-kws))
+                      (mapcar (lambda (s)
+                                (erase-buffer)
+                                (insert s)
+                                (font-lock-ensure)
+                                (buffer-substring (point-min) (point-max)))
+                              import-strings))))))
+    (save-excursion
+      (beginning-of-buffer)
+      (when (re-search-forward "\"\"\"" nil t)
+        (forward-sexp)
+        (forward-line))
+      (while (re-search-forward (rx point (or (seq "package" (* nonl) eol)
+                                              (seq "import" (* nonl) eol)
+                                              eol)) nil t)
+        (forward-line)
+        (beginning-of-line))
+      (forward-line -1)
+      (unless (string= (thing-at-point 'line) "\n")
+        (forward-line 1))
+      (insert choice "\n"))))
 
 (provide 'wildcard-importer)
 
